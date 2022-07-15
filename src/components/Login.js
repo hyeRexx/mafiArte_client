6e8d7063a5ba943ccddf7e5e838f194e3d2e2e79
JackLee9437 <iacog@kakao.com>
2022-07-16 00:42:48 +0900

    add Login component and switching button in main

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link, Route, Routes} from 'react-router-dom'
+
+const Login = () => {
+    return (
+        <div id="login">
+            로그인 컴포넌트
+        </div>
+    );
+}
+
+const Button = styled.button`
+    padding: 2em;
+`
+
+export default Login;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import {React, useState} from 'react';
 import styled from 'styled-components';
-import {Link, Route, Routes} from 'react-router-dom'
+import {Link, Route, Routes} from 'react-router-dom';
+import Login from './Login';
 
 const Main = () => {
+    const [login, setLogin] = useState(false);
+    const flipLogin = () => {
+        setLogin(!login);
+    }
     return (
         <div id="main">
             <h1>메인화면임</h1>
             <h1>메인 화면 및 소셜 로그인 들어가야함</h1>
             <h1>로그인 완료시 로비로 이동</h1>
-            <Button id="join">JOIN</Button>
-            <Link to="/login">
-                <Button id="login">LOGIN</Button>
-            </Link>
-
+            {login? <Login/> : <MainBtns flipLogin={flipLogin}/>}
         </div>
+    );
+}
+
+const MainBtns = (props) => {
+    const btnLogin = () => {
+        props.flipLogin();
+    }
+    
+    return (
+        <>
+            <Button id="join">JOIN</Button>
+            <Button id="login" onClick={btnLogin}>LOGIN</Button>
+        </>
     );
 }
 
