2332da920c0d264e479c51fe6c3195af31266f1c
haein-side <kimhi1640@gmail.com>
2022-07-22 14:54:50 +0900

    해인 invitation 기능 거의 완료 및 밤 기능 구현 중

--- src/components/Ingame.js
+++ src/components/Ingame.js
@@ -10,12 +10,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 
 const Ingame = ({roomId}) => {
     const [ roomEntered, setRoomEntered ] = useState(false);
 
     let friendlist;
+    let [becomeNight, becomeNightState] = useState(false); // 밤이 되었습니다
+    let [voteModal, voteModalState] = useState(false); // 투표 모달
+    let [answerModal, answerModalState] = useState(""); // 마피아 정답 작성 모달
 
     const myId = useSelector(state => state.user.id);
     
@@ -44,9 +50,9 @@ const Ingame = ({roomId}) => {
         });
 
         // start 가능 알림 : for host 
-        socket.on("reayToStart", (data) => {
+        socket.on("readyToStart", (data) => {
             // data : readyToStart : true
-            console.log("debug : reayToStart :", data);
+            console.log("debug : readyToStart :", data);
         });
 
         // game 시작 알림, 첫 번째 턴을 제공받음
@@ -63,17 +69,28 @@ const Ingame = ({roomId}) => {
             console.log("debug : singleTurnInfo :", data);
         });
 
+        /* 밤이 되었습니다 화면 띄우고 투표 / 정답 입력 띄우기 */
         // 한 사이클이 끝났음에 대한 알림
         // data 없음! : turn info도 전달하지 않음
         socket.on("cycleClosed", () => {
+            becomeNightState(true); // 밤이 되었습니다 화면에 출력
+
+            // 시민 : 투표
+            // 마피아 : 제시어 작성
+            voteModalState(true);  // 투표 모달 -> 역할에 따라 분기처리 다르게
+            answerModalState(true); // 마피아 정답 작성 모달
+        
+
             console.log("debug : cycleClosed!")
         });
 
+        /* nightResult 결과를 받음 */
         // nightEvent 요청에 대한 진행 보고
         socket.on("nightResult", (data) => {
             // data : win : mafia or citizen or null
-            //        elected : killed of null
+            //        elected : killed of null : 죽은 시민의 id
             //        voteData : voteData object orray
+            // data.voteData.userId : data.voteData.vote
             console.log("debug : nightResult :", data);
         });
 
@@ -87,6 +104,16 @@ const Ingame = ({roomId}) => {
 
         /*** for game : hyeRexx : end ***/
     },[]);
+
+    /* 밤이 되었습니다 효과 3.5초간 지속 */
+    useEffect(()=> {
+        if (becomeNight) {
+            const showingTimer = setTimeout(()=> {
+                becomeNightState(false);
+            }, 3500);
+            return () => clearTimeout(showingTimer);
+        }
+    }, [becomeNight])
 
     const readyBtn = () => {
         console.log("ready?")
@@ -103,6 +130,9 @@ const Ingame = ({roomId}) => {
         socket.emit("openTurn", {gameId: roomId, userId: myId});
     }
 
+    /* 투표 완료 (nightWork)
+       night work 마친 유저들이 클릭하는 버튼 이벤트
+       투표 혹은 제시어 제출 완료 시 완료 버튼 클릭 후 emit */
     const nightBtn = () => {
         // submit myId는 임시값!
         socket.emit("nightEvent", {gameId: roomId, userId: myId, gamedata: {submit: myId}});
@@ -112,16 +142,25 @@ const Ingame = ({roomId}) => {
         socket.emit("newCycleRequest", {gameId: roomId, userId: myId});
     }
 
+    /* 제시어 제출 - Close 클릭 시 상태 변경 */
+    const btnAnswerClose = () => {
+        answerModalState(false); 
+    };
+
     return (
 
-    <>
+    <>let 
     {
         // 서버쪽에서 접속확인하고 처리
         roomEntered ?
         function () {
             return (
+                <div>
+                    { becomeNight ?  <p className={style.topright}>🌙밤이 되었습니다🌕</p> : null }
+                    {
+                     answerModal ? <AnswerModal className={style.inviteModal} roomId={roomId} myId={myId} btnAnswerClose={btnAnswerClose} /> : null
+                    } 
                 <div className={style.flexBox}>
-                    <p>룸넘버 {roomId}</p>
                     <div className={style.item1}>
                         <VideoWindow />
                     </div>
@@ -147,27 +186,38 @@ const Ingame = ({roomId}) => {
                             </Container>
                         </Navbar>
                         </div>
-                        <div className={style.canvaschat}>
-                            <div className={style.canvas}>
-                                <Canvas roomId={roomId}/>
+                        <div>
+                            <div className={style.canvaschat}>
+                                <div className={style.canvas}>
+                                    <Canvas roomId={roomId}/>
+                                </div>
                             </div>
-                        </div>
 
-                        {/* for gamelogic test */}
-                        <div className="btnbox" style={{position: 'absolute', top: '34%', left: '32%'}}>
-                            <button style={{fontSize: 40, margin: 30}} onClick={readyBtn}> READY </button>
-                            <button style={{fontSize: 40, margin: 30}} onClick={startBtn}> START </button>
-                            <button style={{fontSize: 40, margin: 30}} onClick={openTurnBtn}> OPEN TURN </button>
-                            <button style={{fontSize: 40, margin: 30}} onClick={nightBtn}> NIGHT </button>
-                            <button style={{fontSize: 40, margin: 30}} onClick={newCycleBtn}> NEW CYCLE </button>
-                        </div>
-                        {/* for gamelogic test */}
+                            {/* for gamelogic test */}
+                            <div className="btnbox" style={{position: 'absolute', top: '34%', left: '32%'}}>
+                                <button style={{fontSize: 40, margin: 30}} onClick={readyBtn}> READY </button>
+                                <button style={{fontSize: 40, margin: 30}} onClick={startBtn}> START </button>
+                                <button style={{fontSize: 40, margin: 30}} onClick={openTurnBtn}> OPEN TURN </button>
+                                <button style={{fontSize: 40, margin: 30}} onClick={nightBtn}> NIGHT </button>
+                                <button style={{fontSize: 40, margin: 30}} onClick={newCycleBtn}> NEW CYCLE </button>
+                            </div>
+                            {/* for gamelogic test */}
 
-                        <div className={style.chat}>
-                            <Chat roomId={roomId}/>
+                            <div className={style.chat}>
+                                <Chat roomId={roomId}/>
+                            </div>
                         </div>
                     </div>
                 </div>
+                </div>
+            //     <>
+            //     <div className={style.stars}></div>
+            //     <div className={style.twinkling}></div> 
+            //    <div className={style.clouds}></div>
+            //    <div className={style.title}>
+            //     <h1>A Dark and Mysterious Night</h1>
+            //     </div>
+            //     </>
             );
         }()
         : null
@@ -175,5 +225,44 @@ const Ingame = ({roomId}) => {
     </>
     );
 }
+
+// 마피아 정답 제출 모달
+function AnswerModal(props){
+    const [show, setShow] = useState(true);
+    const handleClose = () => {setShow(false); props.btnAnswerClose();};
+    let [answer, answerState] = useState(''); // 마피아의 제시어 제출
+
+    const submitAnswer = () => {
+        socket.emit("nightEvent", {gameId: props.roomId, userId: props.myId, gamedata: {submit: answer}});
+    }
+
+    return(
+    <>
+        <Modal className={style.modal} style={{ top: "650px" }}  show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>ANSWER</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body className={style.modalBody}>
+                <Form>
+                    <Form.Group className={style.mb3} style={{ marginTop: 10 }}>
+                        <Form.Label className={style.label} >제시어는 무엇일까요?</Form.Label>
+                        <Form.Control
+                            className={style.joinForm}
+                            placeholder="제시어는 무엇일까요?"
+                            autoFocus
+                            onChange={(e) => answerState(e.target.value)}
+                        />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+
+            <Modal.Footer className={style.modalFooter}>
+                <Button variant="primary" onClick={submitAnswer}>SUBMIT</Button>
+            </Modal.Footer>
+        </Modal>
+    </>
+    )
+};
 
 export default Ingame;
